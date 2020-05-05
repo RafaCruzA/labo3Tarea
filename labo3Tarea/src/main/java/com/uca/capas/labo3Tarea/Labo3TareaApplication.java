1499e81a7bf9b8e082dba8d1aba45574820264cb
RafaCruzA <48972341+RafaCruzA@users.noreply.github.com>
2020-05-05 15:03:31 -0600

    Esqueleto

---
+++ labo3Tarea/src/main/java/com/uca/capas/labo3Tarea/Labo3TareaApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas.labo3Tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo3TareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labo3TareaApplication.class, args);
+	}
+
+}
---
+++ labo3Tarea/src/test/java/com/uca/capas/labo3Tarea/Labo3TareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.labo3Tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo3TareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
